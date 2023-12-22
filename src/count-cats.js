a4a709c39ca209be870a351c114642d876b19877
Ivan <ivankonovalov96@inbox.ru>
2023-12-23 00:49:06 +0500

    init: start task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(matrix) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
