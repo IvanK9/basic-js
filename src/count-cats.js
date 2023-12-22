7d3db70a557774069d32fc7ea09cfd95b04debc2
Ivan <ivankonovalov96@inbox.ru>
2023-12-23 00:56:51 +0500

    fix: update file

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,7 +15,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  throw new NotImplementedError('Not implemented');
+  let count = 0;
+  matrix.forEach(el => {
+  el.forEach(item => {
+    if (item == '^^') {
+      count++;
+    }
+  })
+})
+return count
   // remove line with error and write your code here
 }
 
